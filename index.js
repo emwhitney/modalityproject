fb45586c76ed4e09f788f051e4b407c437ee0ec4
lateral-neck-stretch <zhang.anthony97@gmail.com>
2023-10-09 17:21:32 -0400

    update index.js to use createRoot instead of ReactDOM.render for compatibility with React 18

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ import React from "react";
 import reactDom from "react-dom";
 import App from "./src/App";
 
-reactDom.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React>
+    <App />
+  </React>
+);
